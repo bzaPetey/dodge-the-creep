11eed578f589aec627bfc596245fe4a935e83ee9
bzaPetey <peter.laliberte@burgzergarcade.net>
2024-08-24 18:24:38 -0400

    tutorial completed

---
+++ README.md
@@ -0,0 +1,9 @@
+### Source
+
+https://docs.godotengine.org/en/stable/getting_started/first_2d_game/index.html
+
+
+### Version
+
+- Godot 4.3
+- gdscript
--- hud.gd
+++ hud.gd
@@ -32,3 +32,11 @@ func show_game_over():
 
 func update_score(score):
 	$ScoreLabel.text = str(score)
+
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
+
+func _on_message_timer_timeout():
+	$Message.hide()
